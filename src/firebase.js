8e95ff14c0b055c5f5901c7a6e7e8960bb99e58c
Chi Vong <chi.vongvien@gmail.com>
2019-04-08 11:43:43 +0200

    Changed env path

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const config = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  databaseURL: process.env.REACT_APP_databaseURL,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId
+  apiKey: process.env.REACT_APP_apiKey || process.env.apiKey,
+  authDomain: process.env.REACT_APP_authDomain || process.env.authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL || process.env.databaseURL,
+  projectId: process.env.REACT_APP_projectId || process.env.projectId,
+  storageBucket: process.env.REACT_APP_storageBucket || process.env.storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId || process.env.messagingSenderId
 };
 firebase.initializeApp(config);
 
